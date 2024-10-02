5b3a67b3e93e9e9501ef09d1fbbacb09df452a58
BendikFortress <bendik@fortress.no>
2024-10-02 11:29:54 +0200

    upd image object-fit handling

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -44,7 +44,7 @@ export default async function Page() {
                 href={urlTwo}
                 className="relative h-full w-1/2 cursor-pointer"
               >
-                <Image src={image} alt={alt} layout="fill" objectFit="cover" />
+                <Image src={image} alt={alt} layout="fill" style={{objectFit:"cover"}} />
                 <button className="text-3xl absolute bottom-0 left-0 p-6 bg-black text-white">
                   {linkTextTwo}
                 </button>
