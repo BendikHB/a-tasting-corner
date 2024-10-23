dfdb39f89214b21d32042a23e08e9c20e6827bf1
BendikFortress <bendik@fortress.no>
2024-10-23 12:02:23 +0200

    wip recommendations

--- app/(site)/recommendations/page.tsx
+++ app/(site)/recommendations/page.tsx
@@ -1,41 +1,33 @@
-import { Module } from "@/components/Module";
-import { ArrowRight } from "@/public/icons/arrow-right";
-import { getPage } from "@/sanity/sanity-utils";
+import { getPage, getPosts } from "@/sanity/sanity-utils";
 import { PortableText } from "next-sanity";
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
+import React, { Suspense } from "react";
+import { ArrowRight } from "@/public/icons/arrow-right";
+import SearchFilterPosts from "@/components/search-filter-posts/search-filter-posts";
+import FilteredPosts from "@/components/filtered-posts";
 
 export default async function Page() {
-  const page = await getPage("beverages");
+  const page = await getPage("matter-of-taste");
+  const posts = await getPosts();
 
   const {
     image,
     alt,
-    altTwo,
-    dark,
-    darkTwo,
     imageTwo,
+    altTwo,
     url,
     linkText,
     urlTwo,
     linkTextTwo,
-    heading,
+    name,
     intro,
-    modules,
   } = page;
-  const textStyles = {
-    dark1: dark ? "text-white" : "text-black",
-    dark2: darkTwo ? "text-white" : "text-black",
-  };
-  const iconColor = {
-    color1: dark ? "#FFF" : "#000",
-    color2: darkTwo ? "#FFF" : "#000",
-  };
 
   return (
     <div className="mx-auto">
       <section className="flex gap-5 p-5 bg-black h-[50vh]">
-        {image && (
+        {image && url && (
           <Link href={url} className="relative w-1/2 cursor-pointer h-full">
             <Image
               src={image}
@@ -46,23 +38,17 @@ export default async function Page() {
             />
             {url && (
               <div className="px-12 py-8 bg-black absolute bottom-0 left-0">
-                <button
-                  className={`${textStyles.dark1} text-3xl flex items-end`}
-                >
+                <button className={`text-white text-3xl flex items-end`}>
                   {linkText}
                   <div className="pb-2 pl-3">
-                    <ArrowRight
-                      width={48}
-                      height={10}
-                      color={iconColor.color1}
-                    />
+                    <ArrowRight width={48} height={10} color={"#fff"} />
                   </div>
                 </button>
               </div>
             )}
           </Link>
         )}
-        {imageTwo && (
+        {imageTwo && urlTwo && (
           <Link href={urlTwo} className="relative w-1/2 h-full cursor-pointer">
             <Image
               src={imageTwo}
@@ -73,16 +59,10 @@ export default async function Page() {
             />
             {urlTwo && (
               <div className="px-12 py-8 bg-black absolute bottom-0 left-0">
-                <button
-                  className={`${textStyles.dark2} text-3xl flex items-end`}
-                >
+                <button className={`text-white text-3xl flex items-end`}>
                   {linkTextTwo}
                   <div className="pb-2 pl-3">
-                    <ArrowRight
-                      width={48}
-                      height={10}
-                      color={iconColor.color2}
-                    />
+                    <ArrowRight width={48} height={10} color={"#fff"} />
                   </div>
                 </button>
               </div>
@@ -91,17 +71,18 @@ export default async function Page() {
         )}
       </section>
       <section className="max-w-4xl mx-auto pt-20">
-        <h1 className="text-center text-6xl mb-5 max-w-md mx-auto">
-          {heading}
-        </h1>
+        <h1 className="text-center text-6xl mb-5 max-w-md mx-auto">{name}</h1>
         <div className="text-center text-xl">
           <PortableText value={intro} />
         </div>
       </section>
-      {modules &&
-        modules.map((m) => {
-          return <Module module={m} key={m._type} />;
-        })}
+
+      <section className="mt-10">
+        <Suspense>
+          <SearchFilterPosts />
+          <FilteredPosts posts={posts} />
+        </Suspense>
+      </section>
     </div>
   );
 }
