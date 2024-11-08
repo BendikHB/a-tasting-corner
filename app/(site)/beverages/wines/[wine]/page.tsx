894cc68e4af9fa074760158ee58287d46777ea3b
BendikFortress <bendik@fortress.no>
2024-11-08 14:55:57 +0100

    new layout for wine page

--- app/(site)/beverages/wines/[wine]/page.tsx
+++ app/(site)/beverages/wines/[wine]/page.tsx
@@ -33,29 +33,44 @@ export default async function Wine({ params }: Props) {
         </div>
       </section>
       <section className="my-16 pl-[10%] mx-auto">
-        <h2 className="text-3xl">About this wine:</h2>
-        <div className="flex text-center gap-12 mt-8">
+        <div className="flex gap-8">
           <div>
-            <h3 className="font-medium">Type</h3>
-            <p className="text-xl">{wine.type}</p>
+            <Image src={wine.image} alt={wine.alt} width={100} height={300} />
           </div>
-          <div>
-            <h3 className="font-medium">Region</h3>
-            <p className="text-xl">{wine.region}</p>
+          <div className="pt-4">
+            {wine.recommended && <p>We recommend this wine</p>}
+            <h2 className="text-3xl">About this wine:</h2>
+            <div className="flex text-center gap-12 mt-8">
+              <div>
+                <h3 className="font-medium">Type</h3>
+                <p className="text-xl">{wine.type}</p>
+              </div>
+              <div>
+                <h3 className="font-medium">Region</h3>
+                <p className="text-xl">{wine.region}</p>
+              </div>
+              <div>
+                <h3 className="font-medium">Vintage</h3>
+                <p className="text-xl">{wine.vintage}</p>
+              </div>
+              <div>
+                <h3 className="font-medium">Score</h3>
+                <p className="text-xl">{wine.rating}</p>
+              </div>
+            </div>
+            <div className="mt-4">
+              <PortableText value={wine.content} />
+              {wine.recommendation && (
+                <div className="mt-4">
+                  <h3 className="font-semibold">Why we recommend this wine</h3>
+                  <PortableText value={wine.recommendation} />
+                </div>
+              )}
+            </div>
           </div>
-          <div>
-            <h3 className="font-medium">Vintage</h3>
-            <p className="text-xl">{wine.vintage}</p>
-          </div>
-          <div>
-            <h3 className="font-medium">Score</h3>
-            <p className="text-xl">{wine.rating}</p>
-          </div>
-        </div>
-        <div className="mt-4">
-          <PortableText value={wine.content} />
         </div>
       </section>
+      <div className="ml-[10%] w-16 border-t-2"></div>
       <section className="flex mb-16 mt-2 pl-[10%] mx-auto">
         <div className="">
           <h2 className="text-3xl mt-3">Similar options:</h2>
